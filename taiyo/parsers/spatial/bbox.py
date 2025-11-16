2ac0a2e94144a0e4404b82bcf46d109246487a7f
KengoA <20113339+KengoA@users.noreply.github.com>
2025-11-16 18:08:06 +0000

    impl filter queries in spatial query parsers

--- taiyo/parsers/spatial/bbox.py
+++ taiyo/parsers/spatial/bbox.py
@@ -1,5 +1,5 @@
 from typing import Literal
-from pydantic import Field
+from pydantic import computed_field
 from ..base import BaseQueryParser
 from taiyo.params import SpatialSearchParamsMixin
 
@@ -9,8 +9,9 @@ class BoundingBoxQueryParser(BaseQueryParser, SpatialSearchParamsMixin):
     Parser for Solr's bbox spatial query parser.
     """
 
-    def_type: Literal["bbox"] = Field(
-        "bbox",
-        alias="defType",
-        description="defType for bbox parser",
-    )
+    _def_type: Literal["bbox"] = "bbox"
+
+    @computed_field(alias="fq")
+    @property
+    def fiter_query(self) -> str:
+        return f"{{!{self._def_type} sfield={self.spatial_field}}}"
--- taiyo/parsers/spatial/geofilt.py
+++ taiyo/parsers/spatial/geofilt.py
@@ -1,5 +1,5 @@
 from typing import Literal
-from pydantic import Field
+from pydantic import computed_field
 from ..base import BaseQueryParser
 from taiyo.params import SpatialSearchParamsMixin
 
@@ -9,8 +9,9 @@ class GeoFilterQueryParser(BaseQueryParser, SpatialSearchParamsMixin):
     Parser for Solr's geofilt spatial query parser.
     """
 
-    def_type: Literal["geofilt"] = Field(
-        "geofilt",
-        alias="defType",
-        description="defType for geofilt parser",
-    )
+    _def_type: Literal["geofilt"] = "geofilt"
+
+    @computed_field(alias="fq")
+    @property
+    def fiter_query(self) -> str:
+        return f"{{!{self._def_type} sfield={self.spatial_field}}}"
