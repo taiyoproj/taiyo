1395d880ac683df19bb9e24e05fe48a41c47fb83
KengoA <20113339+KengoA@users.noreply.github.com>
2025-11-25 01:50:41 +0000

    ignore assignment of property str

--- taiyo/parsers/spatial/geofilt.py
+++ taiyo/parsers/spatial/geofilt.py
@@ -121,6 +121,6 @@ class GeoFilterQueryParser(SpatialQueryParser):
 
     @computed_field(alias="fq")
     def filter_query(self) -> str:
-        params: str = self.spatial_params()
+        params: str = self.spatial_params  # type: ignore[assignment]
         params_str = f" {params}" if params else ""
         return f"{{!{self.filter_type} sfield={self.spatial_field}{params_str}}}"
